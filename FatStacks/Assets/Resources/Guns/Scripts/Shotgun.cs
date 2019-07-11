5db6ce3bfb9e4189fab459541c448b8101c4a911
BigBoyBonanza <47259396+BigBoyBonanza@users.noreply.github.com>
2019-07-11 13:54:12 -0700

    Adding Shotgun Scripting. Using Freeze gun as basis

---
+++ FatStacks/Assets/Resources/Guns/Scripts/Shotgun.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class Shotgun : Gun
+{
+    public override void fire1(Ray ray)
+    {
+        //RoomResetterInteraction.canReset = true;
+        RaycastHit hit_info;
+        bool object_found = Physics.Raycast(ray, out hit_info, float.MaxValue, LayerMask.GetMask("Default", "InteractSolid"));
+        Box box = hit_info.transform?.GetComponent<Box>();
+    }
+    public override bool canFire()
+    {
+        return ammo > 0;
+    }
+}
