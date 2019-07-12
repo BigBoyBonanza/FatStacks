75201878a56da38a87726798a7092e671c86bcb4
BigBoyBonanza <47259396+BigBoyBonanza@users.noreply.github.com>
2019-07-12 01:05:51 -0700

    Added SFX and commented out current shotgun code. Guide used might not be all that great for current stuff.

--- FatStacks/Assets/Resources/Guns/Scripts/Shotgun.cs
+++ FatStacks/Assets/Resources/Guns/Scripts/Shotgun.cs
@@ -4,12 +4,35 @@ using UnityEngine;
 
 public class Shotgun : Gun
 {
+    public int pelletCount;
+    public float spreadAngle;
+    public GameObject pellet;
+    public float pelletFireVelocity = 1;
+    //setup barrelexit unless it just works
+    List<Quaternion> pellets;
+
+    /*
+    private void Awake()
+    {
+        pellets = new List<Quaternion>(pelletCount);
+        for(int i = 0; i < pelletCount; i++)
+        {
+            pellets.Add(Quaternion.Euler(Vector3.zero));
+        }
+    }
+    */
+
     public override void fire1(Ray ray)
     {
-        //RoomResetterInteraction.canReset = true;
-        RaycastHit hit_info;
-        bool object_found = Physics.Raycast(ray, out hit_info, float.MaxValue, LayerMask.GetMask("Default", "InteractSolid"));
-        Box box = hit_info.transform?.GetComponent<Box>();
+        /*
+        for (int i = 0; i < pelletCount; i++)
+        {
+            pellets[i] = Random.rotation;
+            GameObject p = Instantiate(pellet, BARRELEXIT.Position, BARRELEXIT.rotation);
+            p.transform.rotation = Quaternion.RotateTowards(p.rotation, pellets[i], spreadAngle);
+            p.GetComponent<RigidBody>().AddForce(p.transform.right * pelletFireVelocity)
+        }
+        */
     }
     public override bool canFire()
     {
