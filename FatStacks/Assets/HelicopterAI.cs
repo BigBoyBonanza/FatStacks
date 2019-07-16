b106f3f0c1b52920c8686227171fcdfe858a01ff
Phischermen <kevfischermen@gmail.com>
2019-07-16 09:47:55 -0700

    Helicopter bounces around room much better now.

--- FatStacks/Assets/HelicopterAI.cs
+++ FatStacks/Assets/HelicopterAI.cs
@@ -20,8 +20,49 @@ public class HelicopterAI : MonoBehaviour
 
     private void OnCollisionEnter(Collision collision)
     {
-        Debug.DrawRay(transform.position, collision.GetContact(0).normal,Color.white,5f);
-        rigidbody.velocity = Vector3.Reflect(rigidbody.velocity.normalized, collision.GetContact(0).normal) * rigidbody.velocity.magnitude;
+        Vector3 direction = Vector3.zero;
+        ContactPoint[] contactPoints = new ContactPoint[1];
+        collision.GetContacts(contactPoints);
+        foreach (ContactPoint contactPoint in contactPoints)
+        {
+            Vector3 collisionPosition = collision.GetContact(0).point;
+            //Y
+            if (collisionPosition.y > transform.position.y)
+            {
+                direction += Vector3.down;
+            }
+            else if (collisionPosition.y < transform.position.y)
+            {
+                direction += Vector3.up;
+            }
+            //X
+            if (collisionPosition.x > transform.position.x)
+            {
+                direction += Vector3.left;
+            }
+            else if (collisionPosition.x < transform.position.x)
+            {
+                direction += Vector3.right;
+            }
+            //Z
+            if (collisionPosition.z > transform.position.z)
+            {
+                direction += Vector3.back;
+            }
+            else if (collisionPosition.z < transform.position.z)
+            {
+                direction += Vector3.forward;
+            }
+        }
+        Rigidbody otherRigidbody = collision.gameObject.GetComponent<Rigidbody>();
+        if (otherRigidbody != null)
+        {
+            otherRigidbody.AddForce(direction * 20, ForceMode.VelocityChange);
+        }
+        direction = direction.normalized;
+        Debug.DrawRay(transform.position, direction,Color.white,5f);
+        //rigidbody.velocity = Vector3.Reflect(rigidbody.velocity.normalized, collision.GetContact(0).normal) * rigidbody.velocity.magnitude;
+        rigidbody.velocity = rigidbody.velocity.magnitude * direction;
     }
 
     // Update is called once per frame
