33e831923f4ce64fbfd69edf49d20a0f9e3d01a1
Phischermen <kevfischermen@gmail.com>
2019-07-31 09:58:33 -0700

    Reorganized some stuff, and fixed a few gliches

--- FatStacks/Assets/LevelResources/ConveyerBeltMalfunction.cs
+++ FatStacks/Assets/LevelResources/ConveyerBeltMalfunction.cs
@@ -8,14 +8,21 @@ public class ConveyerBeltMalfunction : MonoBehaviour
     public AudioSource malfunctioning;
     public BoxSpawner[] spawners;
 
+    public bool notTriggered = true;
+
     private void OnTriggerEnter(Collider collision)
     {
-        crash.Play();
-        malfunctioning.Play();
-        foreach (BoxSpawner spawner in spawners)
+        if (notTriggered)
         {
-            spawner.TurnSpawnerOn(true);
+            crash.Play();
+            malfunctioning.Play();
+            foreach (BoxSpawner spawner in spawners)
+            {
+                spawner.TurnSpawnerOn(true);
+            }
+            notTriggered = false;
         }
-        Destroy(gameObject);
+        
+        //Destroy(gameObject);
     }
 }
