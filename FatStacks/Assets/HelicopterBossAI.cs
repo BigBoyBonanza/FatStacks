91b71a731109bdde3e5e31007bea0a648a9ef69b
Phischermen <kevfischermen@gmail.com>
2019-08-19 18:07:40 -0700

    Lots more work gone into boss fight
    I built the basis for the arena. The Helicopter Boss AI has been upgraded to actively try to aim at the Player.

--- FatStacks/Assets/HelicopterBossAI.cs
+++ FatStacks/Assets/HelicopterBossAI.cs
@@ -6,10 +6,12 @@ public class HelicopterBossAI : MonoBehaviour
 {
     public bool moveClockwise = false;
     public int speed;
+    public float range;
     public Transform Helicopter;
     public Transform Player;
     public Bazooka[] bazookas;
     Coroutine TurnCoroutine;
+    bool canTurn = true;
     public Projectile rocket;
     public float fireRate;
     State currState = State.flyingForward;
@@ -17,7 +19,7 @@ public class HelicopterBossAI : MonoBehaviour
     {
         inActive,
         flyingForward,
-        turning
+        flyingForwardAndAttacking,
     };
 
     private void Start()
@@ -38,40 +40,64 @@ public class HelicopterBossAI : MonoBehaviour
             case State.flyingForward:
                 //Go forward
                 transform.Rotate((moveClockwise ? Vector3.up : Vector3.down) * speed * Time.deltaTime);
-                //Check if Player is behind helicopter.
-                if (IsPlayerFacingHelicopter(true, 0.7f))
+                //Check if Player enters range of helicopter.
+                if (IsPlayerFacingHelicopter(true, 0.8f) && canTurn)
                 {
                     TurnCoroutine = StartCoroutine("Turn180Degrees",1f);
+                    currState = State.flyingForwardAndAttacking;
+                }
+                //Rotate back to normal orientation
+                Quaternion dest = (moveClockwise) ? Quaternion.Euler(0, 180, 0) : Quaternion.Euler(0, 0, 0);
+                Helicopter.transform.localRotation = Quaternion.Slerp(Helicopter.localRotation, dest, 0.01f);
+                if (IsPlayerFacingHelicopter(false, 0.6f) && Vector3.Distance(Helicopter.position,Player.position) < range)
+                {
+                    currState = State.flyingForwardAndAttacking;
+                   
                 }
                 break;
-            case State.turning:
+            case State.flyingForwardAndAttacking:
+                //Go forward
+                transform.Rotate((moveClockwise ? Vector3.up : Vector3.down) * speed * Time.deltaTime);
+                //Rotate towards player
+                if (Vector3.Distance(Helicopter.position, Player.position) < range)
+                {
+                    Helicopter.transform.rotation = Quaternion.Slerp(Helicopter.rotation, Quaternion.LookRotation(Helicopter.position - Player.position, Vector3.up), 0.01f);
+                }
+                else
+                {
+                    currState = State.flyingForward;
+                }
                 break;
         }
     }
-
+    
     IEnumerator Turn180Degrees(float rotTime)
     {
-        currState = State.turning;
-        float increment = 180 / rotTime;
-        float progress = 0;
-        Vector3 direction = (moveClockwise ? Vector3.up : Vector3.down);
-        while (progress < 180)
-        {
-            Vector3 v = direction * increment * Time.deltaTime;
-            progress += v.magnitude;
-            Helicopter.transform.Rotate(v);
-            yield return new WaitForSeconds(Time.deltaTime);
-        }
         moveClockwise = !moveClockwise;
-        currState = State.flyingForward;
+        canTurn = false;
+        yield return new WaitForSeconds(rotTime);
+        canTurn = true;
+        //currState = State.turning;
+        //float increment = 180 / rotTime;
+        //float progress = 0;
+        //Vector3 direction = (moveClockwise ? Vector3.up : Vector3.down);
+        //while (progress < 180)
+        //{
+        //    Vector3 v = direction * increment * Time.deltaTime;
+        //    progress += v.magnitude;
+        //    Helicopter.transform.Rotate(v);
+        //    yield return new WaitForSeconds(Time.deltaTime);
+        //}
+        //moveClockwise = !moveClockwise;
+        //currState = State.flyingForward;
     }
-
+    
     IEnumerator FireGuns()
     {
         while (true)
         {
             yield return new WaitForSeconds(fireRate);
-            yield return new WaitUntil(() => IsPlayerFacingHelicopter(false,0.7f) == true);
+            yield return new WaitUntil(() => IsPlayerFacingHelicopter(false,0.9f) == true);
             foreach(Bazooka bazooka in bazookas)
             {
                 bazooka.fire1(new Ray());
