b30ee529aec1490821d9be2d5694e5a152ad2be1
Phischermen <kevfischermen@gmail.com>
2019-08-21 20:27:53 -0700

    Got a bunch of assets in the game.
    *whew*

--- FatStacks/Assets/RocketCrateUnpacker.cs
+++ FatStacks/Assets/RocketCrateUnpacker.cs
@@ -4,23 +4,41 @@ using UnityEngine;
 
 public class RocketCrateUnpacker : MonoBehaviour
 {
-    public Transform rocketSpawnLocation;
-    public string rocketAmmoResourcePath;
+    public Transform itemSpawnLocation;
+    public GameObject rocketAmmo;
+    public GameObject healthPack;
 
     private void OnTriggerEnter(Collider other)
     {
         Box box = other.gameObject.GetComponent<Box>();
-        if(box != null && box.i_am == "RocketAmmo")
+        if(box != null)
         {
-            SpawnRocketAmmo();
-            Destroy(box.gameObject);
+            switch (box.i_am)
+            {
+                case "RocketAmmo":
+                    SpawnRocketAmmo();
+                    Destroy(box.gameObject);
+                    break;
+                case "Health":
+                    SpawnHealth();
+                    break;
+                default:
+                    Destroy(gameObject);
+                    break;
+            }
         }
+        
     }
-
     private void SpawnRocketAmmo()
     {
-        GameObject ammo = (GameObject)Resources.Load(rocketAmmoResourcePath);
-        ammo.transform.position = rocketSpawnLocation.position;
-        Instantiate(ammo);
+        GameObject ammo = Instantiate(rocketAmmo);
+        ammo.transform.position = itemSpawnLocation.position;
+        
+    }
+
+    private void SpawnHealth()
+    {
+        GameObject health = Instantiate(healthPack);
+        health.transform.position = itemSpawnLocation.position;
     }
 }
