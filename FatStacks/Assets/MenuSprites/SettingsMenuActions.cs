fcacf366697bdcdb4bd0dec899b25ac838c117b4
Phischermen <kevfischermen@gmail.com>
2019-08-24 16:06:55 -0700

    Implemented Juliets new icons. Added FOV slider. Options should initialize now without having to go into the settings menu

--- FatStacks/Assets/MenuSprites/SettingsMenuActions.cs
+++ FatStacks/Assets/MenuSprites/SettingsMenuActions.cs
@@ -6,20 +6,23 @@ using UnityEngine;
 public class SettingsMenuActions : MonoBehaviour
 {
     public GameObject mainMenu;
-    public Slider slider;
+    public Slider sliderSensitivity;
+    public Slider sliderFieldOfView;
 
     private void Start()
     {
-        if (!PlayerPrefs.HasKey("Sensitivity"))
-        {
-            PlayerPrefs.SetFloat("Sensitivity", 1f);
-        }
-        slider.value = PlayerPrefs.GetFloat("Sensitivity");
+        sliderSensitivity.value = PlayerPrefs.GetFloat("Sensitivity");
+        sliderFieldOfView.value = PlayerPrefs.GetFloat("FOV");
     }
 
     public void SensitivityChanged(float value)
     {
         PlayerPrefs.SetFloat("Sensitivity", value);
+    }
+
+    public void FieldOfViewChanged(float value)
+    {
+        PlayerPrefs.SetFloat("FOV", value);
     }
 
     public void BackPressed()
